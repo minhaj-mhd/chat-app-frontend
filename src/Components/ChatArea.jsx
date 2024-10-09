dea97cfc32c3e043b063b2e346069ce1d2f0d04c
minhaj-mhd <minhajmuhamd@gmail.com>
2024-10-09 16:38:13 +0530

    added emoji and fetch previous messages from redis

--- src/Components/ChatArea.jsx
+++ src/Components/ChatArea.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect, useState}from 'react'
+import React,{useEffect, useState,useRef}from 'react'
 import Messages from "./Messages"
 import "./ChatArea.css"
 import MessageInput from './MessageInput'
@@ -8,17 +8,40 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
 import SendIcon from '@mui/icons-material/Send';
-
+import EmojiPicker from 'emoji-picker-react';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import zIndex from '@mui/material/styles/zIndex'
 
 function ChatArea({chat_user}) {
   const [wsInstance, setWsInstance] = useState(null)
   const [inputValue, setinputValue] = useState()
   const [messagesdata, setMessagesdata] = useState([]);  // To store all sent and received messages
+  const [emojiOpen, setemojiOpen] = useState(false)
+  const emojiPickerRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (emojiPickerRef.current && !emojiPickerRef.current.contains(event.target)) {
+        setemojiOpen(false); // Close emoji picker
+      }
+    };
+        // Add event listener when emoji picker is open
+        if (emojiOpen) {
+          document.addEventListener('mousedown', handleClickOutside);
+        }
+    
+        // Clean up the event listener when component unmounts or emojiOpen changes
+        return () => {
+          document.removeEventListener('mousedown', handleClickOutside);
+        };
+      }, [emojiOpen]);
+    
 
   useEffect(() => {
     const token = getTokenFromCookies()
     
     const socket = new WebSocket(`ws://127.0.0.1:8000/ws/chat/${chat_user}/?token=${token}`);
+    setMessagesdata([])
     socket.onopen = () => {
       console.log('WebSocket connection established');
     };
@@ -44,6 +67,8 @@ function ChatArea({chat_user}) {
     };
   },[chat_user])
 
+  
+
   const sendMessages = () =>{
 
       const messageData = { message: inputValue,receiver:chat_user };
@@ -53,6 +78,12 @@ function ChatArea({chat_user}) {
       console.log("send message:",inputValue)
       setinputValue("")
     
+  }
+
+  const onEmojiClickfn =(emojiObject,event)=>{
+    console.log(emojiObject.emoji)
+    console.log(emojiObject)
+    setinputValue(inputValue+emojiObject.emoji)
   }
 
 
@@ -67,20 +98,32 @@ function ChatArea({chat_user}) {
         <div className="messages">
 
           {messagesdata.map((obj)=>(
-            <div className="">{obj.receiver===chat_user?<Messages text={obj.text} sent/>: <Messages text={obj.text} />} </div>))}
+            <div key={obj.id} className="">{obj.receiver===chat_user?<Messages text={obj.text} sent/>: <Messages text={obj.text} />} </div>))}
           
            
             
             
         </div>
+        <div ref={emojiPickerRef}className='emoji-select' sx={{zIndex:"10"}}>
+          <EmojiPicker open={emojiOpen} onEmojiClick={onEmojiClickfn}/>
+          </div>
         <div className="input-area">
-        <div className="message-input">
-     <TextField id="standard-basic" label="Type your message." variant="standard" value={inputValue}
-       onChange ={(e)=>setinputValue(e.target.value)} sx={{width:"60%"}}/>
-     <Button variant="contained" endIcon={<SendIcon />} sx={{width:"10%"}} onClick={sendMessages}>
-        Send
-      </Button>
-    </div>
+        
+        <div className="message-input" >
+          <TextField id="standard-basic" label="Type your message." variant="standard" value={inputValue} className='text-input'
+          onChange ={(e)=>setinputValue(e.target.value)} sx={{width:"70%"}}/> 
+          
+          <div className="buttons">
+          
+          <Button onClick={()=>setemojiOpen(!emojiOpen)} className='item'>
+            <EmojiEmotionsIcon/>
+          </Button>
+
+          <Button className='item' variant="contained" endIcon={<SendIcon />}  onClick={sendMessages}>
+          Send
+          </Button>
+          </div>
+        </div>
         </div>
     </div>
   )
